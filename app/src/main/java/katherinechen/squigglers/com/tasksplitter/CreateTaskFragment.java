2cb6e29a3911c0212c403d3f6465daa116ab8e3b
Katherine Chen <squigyx@gmail.com>
2014-11-04 09:46:11 -0600

    create task saves the state of the dropdown menu

--- app/src/main/java/katherinechen/squigglers/com/tasksplitter/CreateTaskFragment.java
+++ app/src/main/java/katherinechen/squigglers/com/tasksplitter/CreateTaskFragment.java
@@ -18,7 +18,13 @@ import android.widget.Toast;
 public class CreateTaskFragment extends Fragment {
     private SessionManager session;
     private DbHelper dbhelper;
-    View view;
+    private View view;
+
+    //for saving info into the bundle onsavedinstancestate
+    private final String GROUPPOSITION = "groupPosition";
+    private final String USERPOSITION = "userPosition";
+    private int groupPosition;
+    private int userPosition;
 
     private Button createTaskButton;
     private Spinner groupSpinner;
@@ -30,9 +36,6 @@ public class CreateTaskFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        session = new SessionManager(getActivity());
-        dbhelper = new DbHelper(getActivity());
     }
 
     @Override
@@ -44,8 +47,8 @@ public class CreateTaskFragment extends Fragment {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
         //assign spinners and textviews
         groupSpinner = (Spinner) view.findViewById(R.id.groupSpinner);
@@ -56,6 +59,17 @@ public class CreateTaskFragment extends Fragment {
         //initially set userSpinner to unclickable and assignToTextView to grey
         userSpinner.setClickable(false);
         assignToTextView.setTextColor(Color.GRAY);
+
+        //get old spinner positions
+        if (savedInstanceState != null) {
+            groupPosition = savedInstanceState.getInt(GROUPPOSITION, -1);
+            userPosition = savedInstanceState.getInt(USERPOSITION, -1);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
 
         setGroupSpinner();  //fills both groups and [users in group] drop down menus
 
@@ -73,8 +87,7 @@ public class CreateTaskFragment extends Fragment {
     }
 
     //adds task to the database
-    private void createTaskProcess()
-    {
+    private void createTaskProcess() {
         //get input fields:
         //taskname and task description
         EditText taskName = (EditText) view.findViewById(R.id.taskName);
@@ -83,20 +96,20 @@ public class CreateTaskFragment extends Fragment {
         String taskdescription = taskDescription.getText().toString().trim();
         //groupId and userId to assign to
         int groupId = -1, userId = -1;
-        if(groupEmpty == false) {   //only get info if group is not empty
+        if (groupEmpty == false) {   //only get info if group is not empty
             Cursor GroupSelection = (Cursor) groupSpinner.getSelectedItem();
             groupId = GroupSelection.getInt(GroupSelection.getColumnIndexOrThrow(DbHelper.Groupy._ID));
 
             Cursor UserSelection = (Cursor) userSpinner.getSelectedItem();
             userId = UserSelection.getInt(UserSelection.getColumnIndexOrThrow(DbHelper.User._ID));
         }
-        
+
         //check that all input fields are correct
         boolean correctInput = inputCheck(taskname, taskdescription, groupId, userId);
 
         //if correctInput = true
         //insert task into database
-        if(correctInput) {
+        if (correctInput) {
             createTask(taskname, taskdescription, groupId, userId);
 
             //toast to show task is created
@@ -117,20 +130,19 @@ public class CreateTaskFragment extends Fragment {
         String error = "";
 
         //if taskname is empty
-        if (taskname.equals("")){
+        if (taskname.equals("")) {
             correctInput = false;
             error = getString(R.string.taskname_empty);
         }
-        
+
         //if user is not in any groups
-        else if(groupEmpty)
-        {
+        else if (groupEmpty) {
             correctInput = false;
             error = getString(R.string.not_in_any_groups);
         }
 
         //show error toast if any incorrect input
-        if(!correctInput)
+        if (!correctInput)
             Toast.makeText(getActivity(), error, Toast.LENGTH_LONG).show();
 
         return correctInput;
@@ -141,13 +153,15 @@ public class CreateTaskFragment extends Fragment {
         //get cursor storing groupId, groupname
         Cursor cursor = dbhelper.getUserGroupsCursor(session.getUserId());
 
-        //set spinner to unclickable if user is not in group (cursor size is 0)
-        if(cursor.getCount() == 0) {
+        //user is not in a group - set spinner to unclickable (cursor size is 0)
+        if (cursor.getCount() == 0) {
             groupSpinner.setClickable(false);
             groupTextView.setTextColor(Color.GRAY);
             groupEmpty = true;
-        }
-        else {
+
+            groupPosition = -1;
+            userPosition = -1;
+        } else {
             //make sure group spinner is clickable
             groupSpinner.setClickable(true);
             groupTextView.setTextColor(Color.BLACK);
@@ -165,6 +179,12 @@ public class CreateTaskFragment extends Fragment {
 
             //set spinner to adapter
             groupSpinner.setAdapter(adapter);
+
+            //set original position of group spinner
+            if(groupPosition != -1 && !groupEmpty)
+                groupSpinner.setSelection(groupPosition);
+            else
+                groupPosition = 0;
         }
 
         //get group selected in order to fill other spinner with users in the group selected
@@ -174,7 +194,13 @@ public class CreateTaskFragment extends Fragment {
                 Cursor c = (Cursor) parent.getItemAtPosition(position);
                 int selectedGroupId = c.getInt(c.getColumnIndexOrThrow(DbHelper.Groupy._ID));
 
-                //fill assignToSpinner
+                //group/user positions
+                int originalGroupPosition = groupPosition;
+                groupPosition = parent.getSelectedItemPosition();   //save position in group drop down menu
+                if(groupPosition != originalGroupPosition)
+                    userPosition = 0;
+
+                //fill user drop down menu
                 setUsersSpinner(selectedGroupId);
             }
 
@@ -205,10 +231,32 @@ public class CreateTaskFragment extends Fragment {
 
         //set spinner to adapter
         userSpinner.setAdapter(adapter);
+
+        userSpinner.setSelection(userPosition); //set user position in spinner
+
+        //saves position of the user drop down menu
+        userSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                userPosition = parent.getSelectedItemPosition();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
-    public void setSession(SessionManager session)
-    {
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(GROUPPOSITION, groupPosition);
+        outState.putInt(USERPOSITION, userPosition);
+    }
+
+    public void setSession(SessionManager session) {
         this.session = session;
     }
 
